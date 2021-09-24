344bbc561050e49b1d597c37a15ec9195741c908
Anand Raghunath <anandr.raghunath@gmail.com>
2021-09-23 22:59:23 -0700

    first commit

---
+++ FtcCode/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HorizontalRight.java
@@ -0,0 +1,149 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+@Autonomous(name="HorizontalRight", group="Training")
+    public class HorizontalRight extends OpMode {
+
+
+
+    DcMotor leftWheel;
+    DcMotor rightWheel;
+    DcMotor backLeftWheel;
+    DcMotor backRightWheel;
+    //DcMotor rightWheel;
+    double drivePower = 0.5;
+
+    @Override
+    public void init() {
+        leftWheel = hardwareMap.dcMotor.get("left_wheel");
+        rightWheel = hardwareMap.dcMotor.get("right_wheel");
+        backRightWheel = hardwareMap.dcMotor.get("back_right_wheel");
+        backLeftWheel = hardwareMap.dcMotor.get("back_left_wheel");
+
+
+
+    }
+    @Override
+    public void loop()  {
+
+        horizontalRight();
+
+
+    }
+
+
+
+    //@Override
+    //public void loop() {
+    //    leftWheel.setPower(drivePower);
+    //    rightWheel.setPower(drivePower);
+    //    backRightWheel.setPower(drivePower);
+    //    backLeftWheel.setPower(drivePower);
+
+
+//    }
+
+
+    public void diagnolLeft() {
+        backLeftWheel.setDirection(DcMotor.Direction.REVERSE);
+        rightWheel.setDirection(DcMotor.Direction.FORWARD);
+
+        rightWheel.setPower(drivePower);
+        backLeftWheel.setPower(drivePower);
+        //sleep(milliseconds 1000);
+
+    }
+
+    public void backwardsDiagnolLeft() {
+        backLeftWheel.setDirection(DcMotor.Direction.FORWARD);
+        rightWheel.setDirection(DcMotor.Direction.REVERSE);
+
+        rightWheel.setPower(drivePower);
+        backLeftWheel.setPower(drivePower);
+        //sleep(2000);
+
+    }
+
+    public void diagnolRight() {
+        backRightWheel.setDirection(DcMotor.Direction.FORWARD);
+        leftWheel.setDirection(DcMotor.Direction.REVERSE);
+
+        leftWheel.setPower(drivePower);
+        backRightWheel.setPower(drivePower);
+        //ElapsedTime(200);
+    }
+
+    public void horizontalRight() {
+        backRightWheel.setDirection(DcMotor.Direction.FORWARD);
+        leftWheel.setDirection(DcMotor.Direction.REVERSE);
+        rightWheel.setDirection(DcMotor.Direction.REVERSE);
+        backLeftWheel.setDirection(DcMotor.Direction.FORWARD);
+
+        leftWheel.setPower(drivePower);
+        backRightWheel.setPower(drivePower);
+        rightWheel.setPower(drivePower);
+        backLeftWheel.setPower(drivePower);
+        //sleep(2000);
+    }
+
+    public void horizontalLeft() {
+        backRightWheel.setDirection(DcMotor.Direction.REVERSE);
+        leftWheel.setDirection(DcMotor.Direction.FORWARD);
+        rightWheel.setDirection(DcMotor.Direction.FORWARD);
+        backLeftWheel.setDirection(DcMotor.Direction.REVERSE);
+
+        leftWheel.setPower(drivePower);
+        backRightWheel.setPower(drivePower);
+        rightWheel.setPower(drivePower);
+        backLeftWheel.setPower(drivePower);
+    }
+
+    public void backwardsDiagnolRight() {
+        backRightWheel.setDirection(DcMotor.Direction.REVERSE);
+        leftWheel.setDirection(DcMotor.Direction.FORWARD);
+
+        leftWheel.setPower(drivePower);
+        backRightWheel.setPower(drivePower);
+
+    }
+
+    public void forward() {
+
+        leftWheel.setDirection(DcMotor.Direction.REVERSE);
+        rightWheel.setDirection(DcMotor.Direction.FORWARD);
+        backLeftWheel.setDirection(DcMotor.Direction.REVERSE);
+        backRightWheel.setDirection(DcMotor.Direction.FORWARD);
+
+        leftWheel.setPower(drivePower);
+        rightWheel.setPower(drivePower);
+        backRightWheel.setPower(drivePower);
+        backLeftWheel.setPower(drivePower);
+
+
+
+
+    }
+
+    public void backward() {
+
+        leftWheel.setDirection(DcMotor.Direction.FORWARD);
+        rightWheel.setDirection(DcMotor.Direction.REVERSE);
+        backLeftWheel.setDirection(DcMotor.Direction.FORWARD);
+        backRightWheel.setDirection(DcMotor.Direction.REVERSE);
+
+        leftWheel.setPower(drivePower);
+        rightWheel.setPower(drivePower);
+        backRightWheel.setPower(drivePower);
+        backLeftWheel.setPower(drivePower);
+
+
+    }
+
+
+
+
+
+}
