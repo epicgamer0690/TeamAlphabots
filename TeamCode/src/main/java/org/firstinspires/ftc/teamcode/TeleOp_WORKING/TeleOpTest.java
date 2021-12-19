74992c4e5f52acfb8395750417ebe959dc1c156a
Suhas Bathini <42387343+sukibaba123aakashbat@users.noreply.github.com>
2021-12-18 16:58:11 -0800

    12/18/21 Field-Centric TeleOp and EasyOpenCV Detection

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp_WORKING/TeleOpTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp_WORKING/TeleOpTest.java
@@ -9,7 +9,7 @@ import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
-@TeleOp(name="TeleOpTest", group="Training")
+@TeleOp(name="TeleOpTest_RoboCentric", group="Training")
     public class TeleOpTest extends OpMode {
 
 
@@ -22,7 +22,7 @@ import com.qualcomm.robotcore.util.ElapsedTime;
     CRServo intakeServo;
     DcMotor carouselMotor;
     double drivePower = 0.5;
-    int rotation = 1000; //1 rotation = 360
+
 
     private ElapsedTime runtime= new ElapsedTime();
 
@@ -81,32 +81,12 @@ import com.qualcomm.robotcore.util.ElapsedTime;
 
         rightWheel.setDirection(DcMotorSimple.Direction.REVERSE); //rightWheel
         backRightWheel.setDirection(DcMotorSimple.Direction.REVERSE); //backRightWheel
+        intakeServo.setDirection(CRServo.Direction.REVERSE);
 
         leftWheel.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         rightWheel.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         backLeftWheel.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         backRightWheel.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);  //setting it so when power is 0, robot stops.
-
-    }
-
-    public void Sleep(int milliseconds) {
-        try {
-            Thread.sleep(milliseconds);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-    }
-    public void resetEncoders() {
-        leftWheel.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        rightWheel.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        backLeftWheel.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        backRightWheel.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-    }
-
-    @Override
-    public void start() {
-
-
 
     }
 
@@ -119,61 +99,30 @@ import com.qualcomm.robotcore.util.ElapsedTime;
         carouselFunc(0.3);
 
     }
-    @Override
-    public void stop() {
-        leftWheel.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        rightWheel.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        backLeftWheel.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        backRightWheel.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
 
-
-    }
-
-
-
-
-
-    //@Override
-    //public void loop() {
-    //    leftWheel.setPower(drivePower);
-    //    rightWheel.setPower(drivePower);
-    //    backRightWheel.setPower(drivePower);
-    //    backLeftWheel.setPower(drivePower);
-
-
-//    }
     public void setButtons(){
-        if(gamepad1.dpad_left == true){
+        if(gamepad1.dpad_left){
             shippingHubLevel(65);
         }
-        if(gamepad1.dpad_right == true){
+        if(gamepad1.dpad_right){
             shippingHubLevel(125 );
         }
-        if(gamepad1.dpad_up == true){
+        if(gamepad1.dpad_up){
             shippingHubLevel(195);
         }
-        if(gamepad1.triangle == true){
-            shippingHubLevelReturn(195);
+        if(gamepad1.dpad_down){
+            shippingHubLevel(50);
+            shippingHubLevel(10);
         }
-        if(gamepad1.circle == true){
-            shippingHubLevelReturn(125);
-        }
-        if(gamepad1.square == true){
-            shippingHubLevelReturn(65);
-        }
-
     }
     public void shippingHubLevel(int rotation) {
         armMotor.setTargetPosition(rotation);
         armMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
         armMotor.setPower(1);
     }
-    public void shippingHubLevelReturn(int rotation){
-        armMotor.setTargetPosition(-rotation);
-        armMotor.setPower(0.04);
-    }
+
     public void carouselFunc(double power){
-        if(gamepad1.cross == true) {
+        if(gamepad1.cross) {
             carouselMotor.setPower(power);
 
         } else {
@@ -182,9 +131,7 @@ import com.qualcomm.robotcore.util.ElapsedTime;
     }
 
     public void intakeFunc() {
-
-        boolean inteeke = gamepad1.left_bumper;
-        if (inteeke == true) {
+        if (gamepad1.left_bumper) {
             intakeServo.setPower(1);
         }
         else{
@@ -193,9 +140,7 @@ import com.qualcomm.robotcore.util.ElapsedTime;
     }
 
     public void outakeFunc(){
-        boolean outeeke;
-        outeeke = gamepad1.right_bumper;
-        if(outeeke == true) {
+        if(gamepad1.right_bumper) {
             intakeServo.setPower(-1);
         }
         else{
@@ -231,182 +176,5 @@ import com.qualcomm.robotcore.util.ElapsedTime;
     }
 
      */
-
-
-    public void diagonalLeft() {
-        /*
-        backLeftWheel.setDirection(DcMotor.Direction.REVERSE);
-        rightWheel.setDirection(DcMotor.Direction.FORWARD);
-
-        rightWheel.setPower(drivePower);
-        backLeftWheel.setPower(drivePower);
-        //sleep(milliseconds 1000);
-
-         */
-
-        rightWheel.setTargetPosition(rotation);
-        backLeftWheel.setTargetPosition(-rotation);
-
-        rightWheel.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        backLeftWheel.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-
-        rightWheel.setPower(drivePower);
-        backLeftWheel.setPower(-drivePower);
-
-
-
-    }
-
-    public void backwardsDiagonalLeft() {
-        /*
-        backLeftWheel.setDirection(DcMotor.Direction.FORWARD);
-        rightWheel.setDirection(DcMotor.Direction.REVERSE);
-
-        rightWheel.setPower(drivePower);
-        backLeftWheel.setPower(drivePower);
-        //sleep(2000);
-
-         */
-
-        rightWheel.setTargetPosition(-rotation);
-        backLeftWheel.setTargetPosition(rotation);
-
-        rightWheel.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        backLeftWheel.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-
-        rightWheel.setPower(-drivePower);
-        backLeftWheel.setPower(drivePower);
-
-    }
-
-    public void diagonalRight() {
-        /*
-        backRightWheel.setDirection(DcMotor.Direction.FORWARD);
-        leftWheel.setDirection(DcMotor.Direction.REVERSE);
-
-        leftWheel.setPower(drivePower);
-        backRightWheel.setPower(drivePower);
-
-         */
-
-        leftWheel.setTargetPosition(-rotation);
-        backRightWheel.setTargetPosition(rotation);
-
-        leftWheel.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        backRightWheel.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-
-        leftWheel.setPower(-drivePower);
-        backRightWheel.setPower(drivePower);
-    }
-
-    public void right() {
-
-        leftWheel.setTargetPosition(-rotation);
-        rightWheel.setTargetPosition(-rotation);
-        backLeftWheel.setTargetPosition(rotation);
-        backRightWheel.setTargetPosition(rotation);
-
-        leftWheel.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        rightWheel.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        backLeftWheel.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        backRightWheel.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-
-        leftWheel.setPower(-drivePower);
-        rightWheel.setPower(-drivePower);
-        backLeftWheel.setPower(drivePower);
-        backRightWheel.setPower(drivePower);
-    }
-
-    public void left() {
-
-        leftWheel.setTargetPosition(rotation);
-        rightWheel.setTargetPosition(rotation);
-        backLeftWheel.setTargetPosition(-rotation);
-        backRightWheel.setTargetPosition(-rotation);
-
-        leftWheel.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        rightWheel.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        backLeftWheel.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        backRightWheel.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-
-        leftWheel.setPower(drivePower);
-        rightWheel.setPower(drivePower);
-        backLeftWheel.setPower(-drivePower);
-        backRightWheel.setPower(-drivePower);
-    }
-
-    public void backwardsDiagonalRight() {
-
-        leftWheel.setTargetPosition(-rotation);
-        backRightWheel.setTargetPosition(rotation);
-
-        leftWheel.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        backRightWheel.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-
-        leftWheel.setPower(-drivePower);
-        backRightWheel.setPower(drivePower);
-
-    }
-
-
-
-    public void forward() {
-
-
-        leftWheel.setTargetPosition(-rotation);
-        rightWheel.setTargetPosition(rotation);
-        backLeftWheel.setTargetPosition(-rotation);
-        backRightWheel.setTargetPosition(rotation);
-
-        leftWheel.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        rightWheel.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        backLeftWheel.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        backRightWheel.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-
-        leftWheel.setPower(-drivePower);
-        rightWheel.setPower(drivePower);
-        backLeftWheel.setPower(-drivePower);
-        backRightWheel.setPower(drivePower);
-
-        leftWheel.setTargetPosition(-300);
-        rightWheel.setTargetPosition(300);
-        backLeftWheel.setTargetPosition(-300);
-        backRightWheel.setTargetPosition(300);
-
-        leftWheel.setPower(-0.2);
-        rightWheel.setPower(0.2);
-        backLeftWheel.setPower(-0.2);
-        backRightWheel.setPower(0.2);
-
-
-
-    }
-
-    public void backward() {
-
-
-        leftWheel.setTargetPosition(rotation);
-        rightWheel.setTargetPosition(-rotation);
-        backLeftWheel.setTargetPosition(rotation);
-        backRightWheel.setTargetPosition(-rotation);
-
-        leftWheel.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        rightWheel.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        backLeftWheel.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        backRightWheel.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-
-        leftWheel.setPower(drivePower);
-        rightWheel.setPower(-drivePower);
-        backLeftWheel.setPower(drivePower);
-        backRightWheel.setPower(-drivePower);
-
-
-
-    }
-
-
-
-
-
 
 }
